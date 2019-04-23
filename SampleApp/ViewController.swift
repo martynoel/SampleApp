0f70bf98b3b4e5ba34714b466340941691663172
Mimi Chenyao <mimichenyao@gmail.com>
2019-04-23 16:58:55 -0700

    Initial Commit

---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Mimi Chenyao on 4/23/19.
+//  Copyright © 2019 Mimi Nadia Chenyao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
